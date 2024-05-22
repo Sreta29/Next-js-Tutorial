f0b484cfa2c14f327096e89c23d3bfa6408cd775
Sreta29 <sreta290101@gmail.com>
2024-05-23 04:49:23 +0800

    Changes in Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Welcome to Home Page</h1>
       <Link href={"/mainmenu"}>Go to Main Menu</Link>
     </>
   );
